32bf2014144d77b5807de347da9f2f6f695be99e
Edgar <edgarmorais@ua.pt>
2019-12-05 16:30:48 +0000

    VideoPlayer working or 444

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,71 +1,75 @@
 import cv2
 import numpy as np
 
-#cap = cv2.VideoCapture('ducks_take_off_444_720p50.y4m')
+'''
+narr = np.array([1,2,3,4,5,6])
+print(narr)
 
+narr = narr.reshape((2,3))
+print(narr)
+
+narr1 = np.array([1,2,3])
+print(narr1.reshape((6,)))
+'''
 f = open('ducks_take_off_444_720p50.y4m','rb')
-
-
+#cap = cv2.VideoCapture('ducks_take_off_444_720p50.y4m')
 status = f.readline()
+print(status)
 status = str(status)[2:-3].split()
 f.readline() #Ler Frame
 width = int(status[1][1:])
 height = int(status[2][1:])
 form = [int((status[-1][1:])[0]),int((status[-1][1:])[1]),int((status[-1][1:])[2])]
 s = sum(form)/4
-print(width)
-print(height)
-print(s)
 y_size = int(width*height)
 uv_size = int(width*height*(form[1]+form[2])/8)
-i=0
-while True:
-    
+
+while(True):
+    #ret, frame = cap.read()
     y = f.read(y_size)
     y = np.frombuffer(y, dtype="uint8")
+    if len(y)==0:
+        break
     u = f.read(uv_size)
     u = np.frombuffer(u, dtype="uint8")
-    u = np.reshape(u, (width*height,))
+    if len(u)==0:
+        break
+    #u = np.reshape(u, (width*height,))
     v = f.read(uv_size)
     v = np.frombuffer(v, dtype="uint8")
-    v = np.reshape(v, (width*height,))
-    #print(v)
-    #print(u)
-    r = np.zeros((width*height,),dtype=np.uint8)
-    g = np.zeros((width*height,),dtype=np.uint8)
-    b = np.zeros((width*height,),dtype=np.uint8)
-    
-    #for i in range(y_size):
-        #r[i] =y[i]+int(1.140*v[i])
-        #print(r)
-        #g[i] = y[i]-0.395*u[i]-0.581*v[i]
-        #b[i] = y[i]+2.032*u[i]
-        #np.append(g,y[i]-0.395*u[i]-0.581*v[i])
-        #np.append(b,y[i]+2.032*u[i])
-    r = y+(1.140*v)
-    g = y-0.395*u-0.581*v
-    b = y+2.032*u
-    #print(r)
-    r = np.reshape(r, (width, height))
-    g = np.reshape(g, (width, height))
-    b = np.reshape(b, (width, height))
-    #print(r)
-    
-    frame = cv2.merge((r,g,b))
-    #print(frame.shape)
-    #print(frame)
-    
-    cv2.imshow('frame',frame)
-    print(frame[5][5])
+    if len(v)==0:
+        break
+    #v = np.reshape(v, (width*height,))
 
+    #r = np.zeros((width*height,),dtype=np.uint8)
+    #g = np.zeros((width*height,),dtype=np.uint8)
+    #b = np.zeros((width*height,),dtype=np.uint8)
+
+    #delta = 128
+    #r = y+(1.403*(u-delta))
+    #g = y-0.714*(u-delta)-0.344*(v-delta)
+    #b = y+1.773*(v-delta)
+
+    y = np.reshape(y, (height,width))
+    u = np.reshape(u, (height,width))
+    v = np.reshape(v, (height,width))
+
+    #r = np.reshape(r, (height,width))
+    #g = np.reshape(g, (height,width))
+    #b = np.reshape(b, (height,width))
+
+    frameexp = cv2.cvtColor(cv2.merge((y,u,v)),cv2.COLOR_YUV2BGR)
+    frameyuv = cv2.merge((y,u,v))
+    #framergb = cv2.merge((r,g,b))
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #print(frameexp.shape)
+    #print(frameexp[0,0])
+    cv2.imshow('frame',frameexp)
+    f.readline()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
     
-#print(f.read(6))
 
 
-#print(str(status)[2:-3])
-#print(f.readline())
-#print(f.readline())
-#print(f.read(1280*720*3))
-#print(f.readline())
-
-#print(f.read(10))
+#cap.release()
+cv2.destroyAllWindows()
